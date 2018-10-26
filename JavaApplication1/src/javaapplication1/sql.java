8fbe3c3592d08ee947a0a6bebdcc43cf001f693b
leonardo <albornozleo95@gmail.com>
2018-10-26 12:05:51 -0430

    interface
    me equivoque...

--- JavaApplication1/src/javaapplication1/sql.java
+++ JavaApplication1/src/javaapplication1/sql.java
@@ -3,19 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package compiladorr;
+package javaapplication1;
+import java.sql.*;
+
 
 /**
  *
  * @author Leonardo
  */
-public class Compiladorr {
-
-    /**
-     * @param args the command line arguments
-     */
+public class sql {
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
